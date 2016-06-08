ac83203cb83cafe41f3ff7c95f8b9abd9978df18
Maribmdj <maribmdj@gmail.com>
2016-06-08 16:21:39 -0600

    Inserts cervezas - compras

---
+++ Scripts/Inserts cerveza.sql
@@ -0,0 +1,9 @@
+INSERT INTO `cerveza` VALUES (1, 'TOURTEL', 'SIN ALCOHOL', 3, 20, 5, 1, 3, 2, 1, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (2, 'AGNUS DAI', 'CON ALCOHOL', 3, 20, 6, 2, 2, 6, 2, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (3, 'AKA MAHAN', '10% ALCOHOL', 5, 15, 3, 2, 4, 1, 3, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (4, 'Altbier', '5.50% ALCOHOL', 6, 16, 6, 2, 1, 17, 3, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (5, 'Andromeda', 'SIN ALCOHOL', 4, 14, 6, 2, 2, 29, 2, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (6, 'Andares', '7% ALCOHOL', 4, 15, 7, 2, 3, 15, 2, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cerveza` VALUES (7, 'Bee Squad', '8,20% ALCOHOL', 4, 10, 6, 2, 4, 31, 2, 'root@localhost','2016-06-08 12:12:50',NULL,NULL);
+
+select * from cerveza;
---
+++ Scripts/Inserts compras.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO `cervezasxpersona` VALUES (1,1,1,'10','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (2,2,1,'15','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (3,3,4,'40','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (4,4,4,'50','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (5,1,6,'10','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (6,2,3,'10','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (7,3,2,'100','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+INSERT INTO `cervezasxpersona` VALUES (8,4,1,'15','root@localhost','2016-06-08 12:12:50',NULL,NULL);
+
+select * from cervezasxpersona;
